75fb2d5fc34de97946b06d748c2356dd3f6c52a4
elchanclasdeltianguis <elchanclas@protonmail.com>
2022-09-16 11:39:53 -0500

    update

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -6,7 +6,7 @@ import useQuicknodeAPINfts from "../hooks/useQuicknodeAPINfts";
 
 import { ENSProfile } from "../components/ENSProfile";
 import QuicknodeTokens from "../components/QuicknodeTokens/QuicknodeTokens";
-import QuicknodeNfts from '../components/QuicknodeNfts/QuicknodeNfts'
+import QuicknodeNfts from "../components/QuicknodeNfts/QuicknodeNfts";
 
 const Home: NextPage = () => {
   const profile = useENSProfile(
@@ -36,14 +36,16 @@ const Home: NextPage = () => {
       <div className="h-24 text-6xl flex justify-center items-center">
         <h1> react tailwind web3 ui kit</h1>
       </div>
-      <main className="pt-8 flex w-full flex-cols items-center gap-8
-                      justify-center  text-center bg-sky-100 space-y-8">
-        <ENSProfile profile={profile} />
-        {loading && <div>Loaading...</div>}
-        {!loading && <QuicknodeTokens balances={tokenBalances} />}
+      <main className="w-full bg-sky-100">
+        {/* components */}
+        <div className="mx-16 flex flex-cols items-center justify-center gap-8">
+          <ENSProfile profile={profile} />
+          {loading && <div>Loaading...</div>}
+          {!loading && <QuicknodeTokens balances={tokenBalances} />}
 
-        {loadingnfts && <div>Loading NFTS</div>}
-        {!loadingnfts && <QuicknodeNfts nfts={nfts} />}
+          {loadingnfts && <div>Loading NFTS</div>}
+          {!loadingnfts && <QuicknodeNfts nfts={nfts} />}
+        </div>
       </main>
     </div>
   );
